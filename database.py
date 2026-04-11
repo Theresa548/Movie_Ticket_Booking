aaaa2f78e203f62ed23ca7900d111d84f2ac7707
Theresa548 <theresamattel548@gmail.com>
2026-04-11 23:51:12 +0530

    Updated database connection

--- database.py
+++ database.py
@@ -1,16 +1,10 @@
-import os
 import mysql.connector
-from urllib.parse import urlparse
 
 def get_db_connection():
-    url = os.getenv("MYSQL_URL")
-
-    result = urlparse(url)
-
     return mysql.connector.connect(
-        host=result.hostname,
-        user=result.username,
-        password=result.password,
-        database=result.path[1:],
-        port=result.port
+        host="metro.proxy.rlwy.net",
+        user="root",
+        password="YOUR_PASSWORD",
+        database="railway",
+        port=26996   # ✅ hardcoded fix
     )
