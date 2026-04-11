4f89fc02d3b7bcec3c37a13eaa5df9a735580b4b
Theresa548 <theresamattel548@gmail.com>
2026-04-11 23:31:22 +0530

    Updated database connection

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 import os
 import mysql.connector
+from urllib.parse import urlparse
 
 def get_db_connection():
+    url = os.getenv("MYSQL_URL")
+
+    result = urlparse(url)
+
     return mysql.connector.connect(
-        host=os.getenv("MYSQLHOST"),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT"))
+        host=result.hostname,
+        user=result.username,
+        password=result.password,
+        database=result.path[1:],
+        port=result.port
     )
