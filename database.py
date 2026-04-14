88a64097d0c898f7c891d2885356da66879be501
Theresa548 <theresamattel548@gmail.com>
2026-04-14 12:11:32 +0530

    Updated database

--- database.py
+++ database.py
@@ -2,10 +2,5 @@ import os
 import psycopg2
 
 def get_db_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT")
-    )
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    return psycopg2.connect(DATABASE_URL)
